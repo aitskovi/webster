c7e93ec5dcb2e0dcfa98b7d92fddc042da6c33b5
Avi Itskovich <avi.itskovich@gmail.com>
2013-05-21 20:50:28 -0400

    Add keys unit tests

--- tests/unit/webster.js
+++ tests/unit/webster.js
@@ -79,5 +79,19 @@ $(function () {
 
         var expectedResult = { 'a': 'ba', 'c': 'da' };
         deepEqual(result, expectedResult);
-    }) 
+    })
+
+    module("keys")
+
+    test("keys should return an empty list for an empty dictionary", function() {
+        var dict = {};
+        var keys = webster.keys(dict);
+        deepEqual(keys, []);
+    })
+
+    test("keys should return all the keys", function() {
+        var dict = { 'a': 'b', 'c': 'd' };
+        var keys = webster.keys(dict);
+        deepEqual(keys, ['a', 'c']);
+    })
 })
