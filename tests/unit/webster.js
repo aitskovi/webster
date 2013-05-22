e75c8128a221fa8188b947348848cbccade06b3b
Avi Itskovich <avi.itskovich@gmail.com>
2013-05-21 20:50:10 -0400

    Add each unit tests

--- tests/unit/webster.js
+++ tests/unit/webster.js
@@ -1,5 +1,55 @@
 $(function () {
 
+    module("each")
+
+    test("each should iterate over all elements", function() {
+        var dict = { 'a': 'b', 'c':'d' };
+        var result = [];
+        webster.each(dict, function(key, value) {
+            result.push(key);
+            result.push(value);
+        });
+
+        deepEqual(result, ["a", "b", "c", "d"]);
+    })
+
+    test("each should be a no-op for an empty dictionary", function() {
+        var dict = {};
+        var modified = false;
+        webster.each(dict, function(key, value) {
+            modified = true;
+        });
+        equal(modified, false);
+    })
+
+    test("each should accept and use a context", function() {
+        var context = { 'item' : 'valid' };
+        var dict = { 'a': 'b', 'c': 'd' };
+        var result = [];
+        webster.each(dict, function(key, value) {
+            result.push(this.item)
+        }, context);
+
+        deepEqual(result, ['valid', 'valid']);
+    });
+
+    test("each shouldn't iterate prototype properties", function() {
+        // TODO(Avi Itskovich): Fix this later
+        /*
+        function Parent() { this.parent = 'parent' };
+        var dict = new Parent();
+        dict['child'] = 'child';
+
+        var result = [];
+        webster.each(dict, function(key, value) {
+            result.push(key);
+            result.push(value);
+        });
+
+        deepEqual(result, ["child", "child"]);
+        */
+    })
+
     module("map")
 
     test("map should create dictionary", function() {
